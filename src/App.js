edbf9fe190a52069c680bbf2554879ae1acf6ae5
Dweina1920 <dina.yamanid@gmail.com>
2022-06-23 00:43:34 +0200

    globo1

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
-import logo from './logo.svg';
+
+import Image from './img/poke-logo.png';
+//---import img from './src/img/poke-logo.png';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="container"> 
+      <header className="header">
+        <div className="header-bg"></div>
+        <img
+          className="header-logo"
+          src={Image}
+          alt="pokemon-logo"></img>
+        <nav className="header-search_bar">
+          <input type="text" name="search" placeholder="Search" ></input>
+          <button type="submit">GO</button>
+        </nav>
       </header>
-    </div>
+      <main className="list-pokemons">
+        <article className="list-pokemons-item">
+          <div className="list-pokemons-item-content">
+            <img
+              src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png"
+              alt="pokemon-1"
+            ></img>
+            <h3>
+              Bulbasaur
+              <span>gras</span>
+            </h3>
+          </div>
+        </article>
+      </main>
+
+      </div>
   );
 }
 
